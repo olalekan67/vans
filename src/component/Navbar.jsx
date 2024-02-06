d2267de938d026b78c7782971106cb87168c391c
bunyamin <bunyaminusalam@gmail.com>
2024-02-06 01:19:12 +0100

    vans website first commit

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../assets/logog.png'
-import signIn from '../assets/icon.png'
+import signIn from '../assets/Icon.png'
 
 
 const Navbar = () => {
